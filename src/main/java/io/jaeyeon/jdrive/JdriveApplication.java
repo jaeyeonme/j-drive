c64990da7eee30733a0bbda4121cc2a822426ad5
Jaeyeon Cho <cjyeon1022@gmail.com>
2024-10-31 12:52:58 +0900

    [#1] Github Actions 설정 추가

---
+++ src/main/java/io/jaeyeon/jdrive/JdriveApplication.java
@@ -0,0 +1,13 @@
+package io.jaeyeon.jdrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdriveApplication.class, args);
+	}
+
+}
