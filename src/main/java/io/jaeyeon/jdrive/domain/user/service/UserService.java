24005d0d6118b0f330045cc7cd7c47791f645ca1
Jaeyeon Cho <cjyeon1022@gmail.com>
2024-11-01 21:12:20 +0900

    [#10] feat: 로그인 DTO 추가

--- src/main/java/io/jaeyeon/jdrive/domain/user/service/UserService.java
+++ src/main/java/io/jaeyeon/jdrive/domain/user/service/UserService.java
@@ -11,7 +11,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 @Service
 @Transactional
 @RequiredArgsConstructor
@@ -50,3 +49,4 @@ public class UserService {
         );
     }
 }
+
